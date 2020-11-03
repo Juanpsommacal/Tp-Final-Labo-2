f1bdc4f7be877e4cc41d19b148cc5d707fb8bee2
Ivancha123 <ivanmetrailler123@gmail.com>
2020-11-02 22:09:23 -0300

    Update Arboles.h

--- Arboles.h
+++ Arboles.h
@@ -18,4 +18,12 @@ nodoArbolstProducto* crearNodoArbol(stProducto producto);
 //Inserta un nodo en el arbol por orden de ID del producto
 nodoArbolstProducto* insertarNodoArbol(nodoArbolstProducto* raiz, stProducto producto);
 
+//Mostrar el nodo
+void mostrar(nodoArbolstProducto * raiz,int modoMostrar);
+
+//Borrar el nodo elegido segun la id del producto seleccionada
+nodoArbolstProducto * borrarNodo(nodoArbolstProducto * nodo,int idProducto);
+
+//Buscar el nodo elegido segun la id del producto seleccionada
+nodoArbolstProducto * buscarPorIdProducto (nodoArbolstProducto * nodo,int idProducto)
 #endif // ARBOLES_H_INCLUDED
